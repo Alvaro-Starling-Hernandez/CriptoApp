18399da13bdc4c021379ec1948f1b30b67efe410
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-24 18:41:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/criptoapp/data/remote/dto/CoinDto.kt
@@ -0,0 +1,8 @@
+package com.example.criptoapp.data.remote.dto
+
+data class CoinDto(
+    val monedaId: String = "",
+    val description: String = "",
+    val valor: String = "",
+    val imageUrl: String = "",
+)
