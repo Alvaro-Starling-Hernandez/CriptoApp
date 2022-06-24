61a7dc34beddd60c2bfd34e8553b2cc495f28b28
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-24 18:49:25 -0400

    Moviendo CoinsApi.kt de directorio

--- app/src/main/java/com/example/criptoapp/data/remote/CoinsApi.kt
+++ app/src/main/java/com/example/criptoapp/data/remote/CoinsApi.kt
@@ -1,5 +1,6 @@
-package com.example.criptoapp.data.remote.dto
+package com.example.criptoapp.data.remote
 
+import com.example.criptoapp.data.remote.dto.CoinDto
 import retrofit2.http.GET
 
 interface CoinsApi {
