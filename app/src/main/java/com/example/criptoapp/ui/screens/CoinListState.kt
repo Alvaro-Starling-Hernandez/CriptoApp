e09f026ccfefef03b458e6cd1629c456e41e1597
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-24 19:50:26 -0400

    Add CoinViewModel.kt

--- app/src/main/java/com/example/criptoapp/ui/screens/CoinListState.kt
+++ app/src/main/java/com/example/criptoapp/ui/screens/CoinListState.kt
@@ -5,6 +5,6 @@ import com.example.criptoapp.data.remote.dto.CoinDto
 
 data class CoinListState(
     val isLoading: Boolean = false,
-    val exchanges: List<CoinDto> = emptyList(),
+    val coins: List<CoinDto> = emptyList(),
     val error: String = ""
 )
---
+++ app/src/main/java/com/example/criptoapp/ui/screens/CoinViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.criptoapp.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.criptoapp.data.remote.CoinsRepository
+import com.example.criptoapp.util.Resource
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+class CoinViewModel @Inject constructor(
+    private val coinsRepository: CoinsRepository
+) : ViewModel() {
+    private val _state = mutableStateOf(CoinListState())
+    val state: State<CoinListState> = _state
+
+    init {
+        coinsRepository.getCoins().onEach { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    _state.value = CoinListState(isLoading = true)
+                }
+                is Resource.Success -> {
+                    _state.value = CoinListState(coins = result.data ?: emptyList())
+                }
+                is Resource.Error -> {
+                    _state.value = CoinListState(error = result.message ?: "Error desconocido")
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
