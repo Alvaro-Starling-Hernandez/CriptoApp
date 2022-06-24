354e72a89ddb6ba9c5813dc0f3790879b62916a8
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-24 18:55:02 -0400

    Add CoinsRepository.kt

---
+++ app/src/main/java/com/example/criptoapp/data/remote/CoinsRepository.kt
@@ -0,0 +1,28 @@
+package com.example.criptoapp.data.remote
+
+import com.example.criptoapp.data.remote.dto.CoinDto
+import com.example.criptoapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class CoinsRepository @Inject constructor(
+    private val api: CoinsApi
+){
+    fun getCoins(): Flow<Resource<List<CoinDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+
+            val exchanges = api.getCoins()
+
+            emit(Resource.Success(exchanges))
+
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.message ?: "Error HTTP general"))
+        } catch (e: IOException) {
+            emit(Resource.Error(e.message ?: "Verificar la conexion a internet"))
+        }
+    }
+}
