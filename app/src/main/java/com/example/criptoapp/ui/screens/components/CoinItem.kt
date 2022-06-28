39d40b6cff24af319c96d4c4a3aead0673442e12
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-28 13:37:33 -0400

    Agregando tipo de fuente y cambiando formato de valor

--- app/src/main/java/com/example/criptoapp/ui/screens/components/CoinItem.kt
+++ app/src/main/java/com/example/criptoapp/ui/screens/components/CoinItem.kt
@@ -12,11 +12,16 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.criptoapp.data.remote.dto.CoinDto
+import com.example.criptoapp.ui.theme.Eczar
+import java.text.DecimalFormat
 
 @Composable
 fun CoinItem(
@@ -35,15 +40,18 @@ fun CoinItem(
             .padding(8.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Row{
+            Row {
                 Image(coin = coin)
                 Spacer(modifier = Modifier.width(15.dp))
                 Text(text = coin.descripcion)
             }
-
+            val decimalFormat = DecimalFormat("#,###.######")
             Text(
-                text = coin.valor,
-                textAlign = TextAlign.End
+                text = "$ " + decimalFormat.format(coin.valor.toDouble()),
+                textAlign = TextAlign.End,
+                fontWeight = FontWeight.Normal,
+                letterSpacing = 3.sp,
+                fontFamily = Eczar
             )
         }
 
--- app/src/main/java/com/example/criptoapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/criptoapp/ui/theme/Type.kt
@@ -2,10 +2,20 @@ package com.example.criptoapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.criptoapp.R
 
+
+val Eczar = FontFamily(
+    Font(R.font.eczar_bold, FontWeight.Bold),
+    Font(R.font.eczar_extrabold, FontWeight.ExtraBold),
+    Font(R.font.eczar_medium, FontWeight.Medium),
+    Font(R.font.eczar_regular, FontWeight.Normal),
+    Font(R.font.eczar_semibold, FontWeight.SemiBold)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
